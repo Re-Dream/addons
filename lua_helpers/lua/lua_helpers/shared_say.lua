5a5de59271fe0038550c513e6943d96f256743ee
Tenrys <tenrys.iaido@live.fr>
2017-03-18 01:18:51 +0100

    should work better

--- lua_helpers/lua/lua_helpers/shared_say.lua
+++ lua_helpers/lua/lua_helpers/shared_say.lua
@@ -1,7 +1,18 @@
 
 if CLIENT then
-	function Say(str)
-		RunConsoleCommand("say", str)
+	function Say(...)
+		local first = true
+		local msg = ""
+		for _, v in pairs({...}) do
+			if first then
+				first = false
+			else
+				msg = msg .. " "
+			end
+			msg = msg .. tostring(v)
+		end
+		msg = msg:gsub("\n", ""):gsub(";", ":"):gsub("\"", "'")
+		LocalPlayer():ConCommand("say " .. msg)
 	end
 elseif SERVER then
 	function Say(...)
