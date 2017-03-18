3f6ed07cc93818d6edb53e9279e02f420039dc0b
Tenrys <tenrys.iaido@live.fr>
2017-03-18 04:00:09 +0100

    removed toolkit and luadev chatcmds since we don't need them anymore added a little more workshop downloads

--- misc/lua/autorun/server/workshop_downloads.lua
+++ misc/lua/autorun/server/workshop_downloads.lua
@@ -1,11 +1,15 @@
 
 local addons = {
 	["prone-mod"] = 775573383,
+	["notifications"] = 650064006, -- to be redone by ourselves
+	["jumppads"] = 431422041,
 }
 local maps = {
 	["gm_bluehills_test3"] = 243902601,
 	["gm_freespace_09_extended"] = 278492798,
 	["gm_freespace_13"] = 115510325,
+	["gm_abstraction_ex-night"] = 741592270,
+	["gm_abstraction_ex-sunset"] = 740691120,
 }
 
 for name, id in next, addons do
