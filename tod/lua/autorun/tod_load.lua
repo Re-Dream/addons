2e36cc63e79327f2918918663855dba0f5782277
Tenrys <tenrys.iaido@live.fr>
2017-03-21 21:10:43 +0100

    order

--- tod/lua/autorun/tod_load.lua
+++ tod/lua/autorun/tod_load.lua
@@ -1,4 +1,7 @@
-AddCSLuaFile("tod/tod.lua")
-include("tod/tod.lua")
+
 AddCSLuaFile("includes/modules/bsp.lua")
 include("includes/modules/bsp.lua")
+
+AddCSLuaFile("tod/tod.lua")
+include("tod/tod.lua")
+
