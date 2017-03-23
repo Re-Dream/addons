f21210490552dd9f74ad7c8e99f0391e7c7626b4
Toasty Engineer <ToastyEngineer@users.noreply.github.com>
2017-03-23 16:57:21 +0100

    tell no one of this

--- mingeban-commands/lua/mingeban/commands/game.lua
+++ mingeban-commands/lua/mingeban/commands/game.lua
@@ -14,11 +14,13 @@ mingeban.CreateCommand("revive", function(caller, line)
 end)
 
 mingeban.CreateCommand("map",function(caller, line)
+	if not caller:IsAdmin() then return end
 	line = line:gsub(".bsp", "")
 	game.ConsoleCommand("changelevel " .. line .. "\n")
 end)
 
 mingeban.CreateCommand("maps",function(caller)
+	if not caller:IsAdmin() then return end
 	for _, v in next, (file.Find("maps/*.bsp", "GAME")) do
 		caller:PrintMessage(HUD_PRINTCONSOLE, v)
 	end
