d61f94db8e8c0b78d8d0b1a43c7faadc4f2a65e5
Tenrys <tenrys.iaido@live.fr>
2017-03-25 17:28:58 +0100

    OOPS

--- misc/lua/autorun/server/tool_abuse_restrict.lua
+++ misc/lua/autorun/server/tool_abuse_restrict.lua
@@ -1,7 +1,7 @@
 
 hook.Add("CanTool", "ToolAbuseRestrict", function(ply, _, tool)
 	if tool == "fading_door" and not table.HasValue(list.Get("FDoorMaterials"), ply:GetInfo("fading_door_mat")) then
-		a:ChatPrint("Restricted to allowed materials only!")
+		ply:ChatPrint("Restricted to allowed materials only!")
 		return false
 	end
 end)
