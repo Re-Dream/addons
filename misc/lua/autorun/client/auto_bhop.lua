40690b13f9ca249382de150250a594998e3431f4
Tenrys <tenrys.iaido@live.fr>
2017-03-28 22:17:13 +0200

    fix

--- misc/lua/autorun/client/auto_bhop.lua
+++ misc/lua/autorun/client/auto_bhop.lua
@@ -3,7 +3,7 @@ local bhop = CreateClientConVar("auto_bhop", "0", true)
 
 hook.Add("CreateMove", "auto_bhop", function(cmd)
 	if not bhop:GetBool() then return end
-	if LocalPlayer():GetActiveWeapon():GetClass() == "weapon_archerbow" then return end
+	if IsValid(LocalPlayer():GetActiveWeapon()) and LocalPlayer():GetActiveWeapon():GetClass() == "weapon_archerbow" then return end
 	if bit.band(cmd:GetButtons(), IN_JUMP) == 2 and LocalPlayer():GetMoveType() ~= MOVETYPE_NOCLIP and LocalPlayer():WaterLevel() <= 1 and not LocalPlayer():OnGround() then
 		cmd:SetButtons(bit.band(cmd:GetButtons(), bit.bnot(IN_JUMP)))
 	end
