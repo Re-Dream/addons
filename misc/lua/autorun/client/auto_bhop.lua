b2146ebed4197a65c32cd45e5df303a79be858d6
Tenrys <tenrys.iaido@live.fr>
2017-03-29 16:33:03 +0200

    i KNEW that looked odd

--- misc/lua/autorun/client/auto_bhop.lua
+++ misc/lua/autorun/client/auto_bhop.lua
@@ -3,7 +3,8 @@ local bhop = CreateClientConVar("auto_bhop", "0", true)
 
 hook.Add("CreateMove", "auto_bhop", function(cmd)
 	if not bhop:GetBool() then return end
-	if IsValid(LocalPlayer():GetActiveWeapon()) and LocalPlayer():GetActiveWeapon():GetClass() == "weapon_archerbow" then return end
+	local wep = LocalPlayer():GetActiveWeapon()
+	if IsValid(wep) and wep:GetClass() == "weapon_archerbow" then return end -- grapplefuck
 	if bit.band(cmd:GetButtons(), IN_JUMP) == 2 and LocalPlayer():GetMoveType() ~= MOVETYPE_NOCLIP and LocalPlayer():WaterLevel() <= 1 and not LocalPlayer():OnGround() then
 		cmd:SetButtons(bit.band(cmd:GetButtons(), bit.bnot(IN_JUMP)))
 	end
