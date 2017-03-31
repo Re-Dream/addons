c93cd02ca4219a3e3cd255fbb75bd535bde40943
Tenrys <tenrys.iaido@live.fr>
2017-03-31 19:33:58 +0200

    that should do it

--- misc/lua/autorun/camera_override.lua
+++ misc/lua/autorun/camera_override.lua
@@ -1,26 +1,30 @@
 
-local SWEP = weapons.GetStored("gmod_camera")
+hook.Add("Initialize", "gmod_camera_override", function()
+	hook.Remove("Initialize", "gmod_camera_override")
 
-SWEP._Reload = SWEP._Reload or SWEP.Reload
+	local SWEP = weapons.GetStored("gmod_camera")
 
-SWEP.NextReload = 0
-function SWEP:Reload()
-	if self.Owner:KeyDown(IN_USE) and self.NextReload < CurTime() then
-		self:SetNWBool("Hidden", not self:GetNWBool("Hidden"))
-		if self:GetNWBool("Hidden") then
-			self:SetHoldType("normal")
-		else
-			self:SetHoldType("camera")
+	SWEP._Reload = SWEP._Reload or SWEP.Reload
+
+	SWEP.NextReload = 0
+	function SWEP:Reload()
+		if self.Owner:KeyDown(IN_USE) and self.NextReload < CurTime() then
+			self:SetNWBool("Hidden", not self:GetNWBool("Hidden"))
+			if self:GetNWBool("Hidden") then
+				self:SetHoldType("normal")
+			else
+				self:SetHoldType("camera")
+			end
+			self.NextReload = CurTime() + 1
+			return
 		end
-		self.NextReload = CurTime() + 1
-		return
+		self:_Reload()
 	end
-	self:_Reload()
-end
 
-function SWEP:DrawWorldModel()
-	if not self:GetNWBool("Hidden") then
-		self:DrawModel()
+	function SWEP:DrawWorldModel()
+		if not self:GetNWBool("Hidden") then
+			self:DrawModel()
+		end
 	end
-end
+end)
 
