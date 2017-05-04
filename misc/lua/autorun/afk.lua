9af20b30504fa98060ed7ef20f2fd911c7d70910
Tenrys <tenrys.iaido@live.fr>
2017-05-04 08:30:22 +0200

    this should fix some stuff like when it sometimes say you've been afk for 8 hours after just logging in, or focusing the game not marking you as being back needs a visual redesign i believe

--- misc/lua/autorun/afk.lua
+++ misc/lua/autorun/afk.lua
@@ -34,12 +34,12 @@ if SERVER then
 elseif CLIENT then
 
 	afk.Mouse = {x = 0, y = 0}
-	afk.When = CurTime() + afk.AFKTime:GetInt()
 	afk.Focus = system.HasFocus()
 	afk.Is = false
 	afk.Network = true
 
 	local function Input()
+		if not afk.When then return end
 		afk.When = CurTime() + afk.AFKTime:GetInt()
 		if afk.Is and afk.Network then
 			net.Start(tag)
@@ -49,17 +49,16 @@ elseif CLIENT then
 		afk.Is = false
 	end
 	hook.Add("StartCommand", tag, function(ply, cmd)
-		if ply ~= LocalPlayer() then return end
-		local mouseMoved = (system.HasFocus() and (afk.Mouse.x ~= gui.MouseX() or afk.Mouse.y ~= gui.MouseY()))
+		if ply ~= LocalPlayer() or not afk.When then return end
+		local mouseMoved = (system.HasFocus() and (afk.Mouse.x ~= gui.MouseX() or afk.Mouse.y ~= gui.MouseY()) or false)
 		if  mouseMoved or
 			cmd:GetMouseX() ~= 0 or
 			cmd:GetMouseY() ~= 0 or
 			cmd:GetButtons() ~= 0 or
-			(afk.When >= CurTime() and not afk.Focus and system.HasFocus())
+			(afk.Focus == false and afk.Focus ~= system.HasFocus())
 		then
 			Input()
 		end
-		afk.Focus = system.HasFocus()
 		if afk.When < CurTime() and not afk.Is then
 			afk.Is = true
 			if afk.Network then
@@ -119,7 +118,9 @@ elseif CLIENT then
 	afk.Draw = CreateConVar("cl_afk_hud_draw", "1", { FCVAR_ARCHIVE }, "Should we draw the AFK HUD?")
 	hook.Add("HUDPaint", tag, function()
 		if not afk.Draw:GetBool() then return end
-		if afk.When >= CurTime() then a = 0 return end
+		if not afk.When then afk.When = CurTime() + afk.AFKTime:GetInt() end
+		afk.Focus = system.HasFocus()
+		if not afk.Is then a = 0 return end
 
 		a = math.Clamp(a + FrameTime() * 120, 0, 255)
 
