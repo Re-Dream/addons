7119bb8013d0fc05ebc963702a215d15282eeb85
Tenrys <tenrys.iaido@live.fr>
2017-05-29 01:30:21 +0200

    Moved the JSON to Lua, I'm dumb. Doing this through PHP limits me

--- misc/lua/autorun/server/github_webhooks.lua
+++ misc/lua/autorun/server/github_webhooks.lua
@@ -3,8 +3,38 @@
 concommand.Add("read_last_commit", function()
 	local exists = file.Exists("last_commit.txt", "DATA")
 	if exists then
-		local text = file.Read("last_commit.txt", "DATA")
-		ChatAddText(Color(155, 255, 64), text)
+		local commit = util.JSONToTable(file.Read("last_commit.txt", "DATA"))
+		local text = {}
+		if commit.head_commit.message and commit.head_commit.message:Trim() ~= "" then
+			local txt = ""
+			local lines = commit.head_commit.message:Trim():Split("\n")
+			for k, v in next, lines do
+				txt = txt .. "    > " .. v .. "\n" -- (k == #lines and "" or "\n")
+			end
+			text[#text + 1] = txt
+		end
+		local operations = {
+			modified = { Symbol = "*", Color = Color(96, 96, 255) },
+			added = { Symbol = "+", Color = Color(96, 255, 96) },
+			removed = { Symbol = "-", Color = Color(255, 96, 96) }
+		}
+		for op, info in next, operations do
+			if #commit.head_commit[op] > 0 then
+				text[#text + 1] = info.Color
+				local txt = ""
+				for k, v in next, commit.head_commit[op] do
+					txt = txt .. "    " .. info.Symbol .. " " .. v .. "\n" -- (k == #commit.head_commit[op] and "" or "\n")
+				end
+				text[#text + 1] = txt
+			end
+		end
+		text[#text] = text[#text]:gsub("\n$", "")
+		ChatAddText(
+			Color(175, 235, 225),
+			"Update in " .. commit.repository.name .. " by " .. commit.head_commit.author.username .. " (" .. commit.head_commit.author.email .. "):\n",
+			unpack(text)
+		)
+
 	end
 end)
 
