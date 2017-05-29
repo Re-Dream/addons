a3818d307bf1fef82b150bce0e701d4afeaf2cfd
Tenrys <tenrys.iaido@live.fr>
2017-05-29 02:17:50 +0200

    ok brain

--- misc/lua/autorun/server/joininfo.lua
+++ misc/lua/autorun/server/joininfo.lua
@@ -1,9 +1,4 @@
 
-local function HandleSharedPlayer(ply)
-		if ply:IsFamilySharing() then
-	end
-end
-
 hook.Add("PlayerAuth", "FamilyShareChecker", function(ply)
 	if ply:IsFamilySharing() then
 		Msg("[FamilySharing]") print(string.format("%s (%s) has been lent Garry's Mod by %s",
