61f9f68aa5f08c8f8ab19fac18848d682f321b17
Tenrys <tenrys.iaido@live.fr>
2017-05-29 03:44:04 +0200

    forgot addcsluafile

--- scoreboard/lua/autorun/scoreboard_load.lua
+++ scoreboard/lua/autorun/scoreboard_load.lua
@@ -1,6 +1,8 @@
 
 if SERVER then
 	AddCSLuaFile("scoreboard/scoreboard.lua")
+	AddCSLuaFile("scoreboard/team_panel.lua")
+	AddCSLuaFile("scoreboard/player_panel.lua")
 else
 	include("scoreboard/scoreboard.lua")
 end
