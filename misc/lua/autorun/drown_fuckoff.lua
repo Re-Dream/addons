6b530e3c0cf151884227a43eacbaa3014e65efbd
Tenrys <tenrys.iaido@live.fr>
2017-10-24 21:51:48 +0200

    fix

--- misc/lua/autorun/drown_fuckoff.lua
+++ misc/lua/autorun/drown_fuckoff.lua
@@ -1,6 +1,6 @@
 
 hook.Add("EntityEmitSound", "drown_fuckoff", function(data)
 	if type(data.Entity) ~= "Player" then return end
-	if data.Entity.isAFK == nil and data.OriginalSoundName == "Player.DrownStart" then return false end
+	if data.Entity.m_bWasOnGround == nil and data.OriginalSoundName == "Player.DrownStart" then return false end
 end)
 
