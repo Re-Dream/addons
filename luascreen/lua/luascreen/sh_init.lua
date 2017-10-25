adaf6d451eebc341340df3c558803eb4b23578f5
Tenrys <tenrys.iaido@live.fr>
2017-10-26 00:08:20 +0200

    oops

--- luascreen/lua/luascreen/sh_init.lua
+++ luascreen/lua/luascreen/sh_init.lua
@@ -2,7 +2,7 @@
 luascreen = {}
 
 AddCSLuaFile("sh_entity.lua")
-AddCSLuaFile("sh_modular.lua")
+AddCSLuaFile("sh_screens.lua")
 include("sh_entity.lua")
-include("sh_modular.lua")
+include("sh_screens.lua")
 
