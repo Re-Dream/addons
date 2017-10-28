aea668f2fa4b5137a7b1d21a57a26d745fd0f16d
Tenrys <tenrys.iaido@live.fr>
2017-10-28 02:10:41 +0200

    off to submodules

--- misc/lua/autorun/server/stormfox_realtime.lua
+++
@@ -1,8 +0,0 @@
-
-hook.Add("Initialize", "stormfox_realtime", function()
-	if not StormFox then return end
-
-	StormFox.SetTime(os.date("%H:%M"))
-	RunConsoleCommand("sf_timespeed", 1 / 60)
-end)
-
--- scoreboard/lua/autorun/scoreboard_load.lua
+++
@@ -1,9 +0,0 @@
-
-if SERVER then
-	AddCSLuaFile("scoreboard/scoreboard.lua")
-	AddCSLuaFile("scoreboard/team_panel.lua")
-	AddCSLuaFile("scoreboard/player_panel.lua")
-else
-	include("scoreboard/scoreboard.lua")
-end
-
